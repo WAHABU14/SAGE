451a2393766b0e1ebabdad31be6df738d7978bd3
WAHABU14 <161591965+WAHABU14@users.noreply.github.com>
2024-02-28 18:22:18 +0300

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("what is your name?")
+age=input("what is your age?")
+location=input("where do you live?")
+print(f"You are name is {name}, You are age is {age} years old and you live at {location}.")
